5ab09e206f9c54b2d13d285533c6894f31c668a8
Rahul Narayanan <rahulon@icloud.com>
2024-09-06 18:41:06 -0400

    Initial Commit

---
+++ CanvasPlusPlayground/CanvasPlusPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasPlusPlaygroundApp.swift
+//  CanvasPlusPlayground
+//
+//  Created by Rahul on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPlusPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
