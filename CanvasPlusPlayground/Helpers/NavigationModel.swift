0a3f83d41ddea33b143add228767ca4d89ea023b
iWatger <ethanfox646@icloud.com>
2024-11-03 01:24:04 -0400

    Saves selected course, still working out selected page

--- CanvasPlusPlayground/Helpers/NavigationModel.swift
+++ CanvasPlusPlayground/Helpers/NavigationModel.swift
@@ -9,12 +9,35 @@ import SwiftUI
 
 @Observable
 class NavigationModel {
-    enum CoursePage {
+    enum CoursePage:String {
         case assignments, files, announcements, grades, calendar, people, tabs
     }
 
     var selectedCourse: Course? {
-        didSet { selectedCoursePage = nil }
+        didSet {
+            let dataRep = try! JSONEncoder().encode(selectedCourse)
+            UserDefaults.standard.set(dataRep, forKey: "selectedCourse")
+            selectedCoursePage = nil
+        }
     }
-    var selectedCoursePage: CoursePage?
+    var selectedCoursePage: CoursePage? {
+        didSet {
+            if let page = selectedCoursePage {
+                UserDefaults.standard.set(page.rawValue, forKey: "selectedPage")
+            } else {
+                UserDefaults.standard.removeObject(forKey: "selectedPage")
+            }
+        }
+    }
+    
+    init() {
+        let courseVal = UserDefaults.standard.data(forKey: "selectedCourse")
+        guard let courseVal else { return }
+        selectedCourse = Course(from: courseVal)
+        
+        if let rawValue = UserDefaults.standard.string(forKey: "selectedPage"),
+           let state = CoursePage(rawValue: rawValue) {
+            selectedCoursePage = state
+        }
+    }
 }
--- CanvasPlusPlayground/Model/Course.swift
+++ CanvasPlusPlayground/Model/Course.swift
@@ -9,117 +9,118 @@
  https://canvas.instructure.com/doc/api/courses.html
  A course object looks like:
  {
-   // the unique identifier for the course
-   "id": 370663,
-   // the SIS identifier for the course, if defined. This field is only included if
-   // the user has permission to view SIS information.
-   "sis_course_id": null,
-   // the UUID of the course
-   "uuid": "WvAHhY5FINzq5IyRIJybGeiXyFkG3SqHUPb7jZY5",
-   // the integration identifier for the course, if defined. This field is only
-   // included if the user has permission to view SIS information.
-   "integration_id": null,
-   // the unique identifier for the SIS import. This field is only included if the
-   // user has permission to manage SIS information.
-   "sis_import_id": 34,
-   // the full name of the course. If the requesting user has set a nickname for
-   // the course, the nickname will be shown here.
-   "name": "InstructureCon 2012",
-   // the course code
-   "course_code": "INSTCON12",
-   // the actual course name. This field is returned only if the requesting user
-   // has set a nickname for the course.
-   "original_name": "InstructureCon-2012-01",
-   // the current state of the course, also known as ‘status’.  The value will be
-   // one of the following values: 'unpublished', 'available', 'completed', or
-   // 'deleted'.  NOTE: When fetching a singular course that has a 'deleted'
-   // workflow state value, an error will be returned with a message of 'The
-   // specified resource does not exist.'
-   "workflow_state": "available",
-   // the account associated with the course
-   "account_id": 81259,
-   // the root account associated with the course
-   "root_account_id": 81259,
-   // the enrollment term associated with the course
-   "enrollment_term_id": 34,
-   // A list of grading periods associated with the course
-   "grading_periods": null,
-   // the grading standard associated with the course
-   "grading_standard_id": 25,
-   // the grade_passback_setting set on the course
-   "grade_passback_setting": "nightly_sync",
-   // the date the course was created.
-   "created_at": "2012-05-01T00:00:00-06:00",
-   // the start date for the course, if applicable
-   "start_at": "2012-06-01T00:00:00-06:00",
-   // the end date for the course, if applicable
-   "end_at": "2012-09-01T00:00:00-06:00",
-   // the course-set locale, if applicable
-   "locale": "en",
-   // A list of enrollments linking the current user to the course. for student
-   // enrollments, grading information may be included if include[]=total_scores
-   "enrollments": null,
-   // optional: the total number of active and invited students in the course
-   "total_students": 32,
-   // course calendar
-   "calendar": null,
-   // the type of page that users will see when they first visit the course -
-   // 'feed': Recent Activity Dashboard - 'wiki': Wiki Front Page - 'modules':
-   // Course Modules/Sections Page - 'assignments': Course Assignments List -
-   // 'syllabus': Course Syllabus Page other types may be added in the future
-   "default_view": "feed",
-   // optional: user-generated HTML for the course syllabus
-   "syllabus_body": "<p>syllabus html goes here</p>",
-   // optional: the number of submissions needing grading returned only if the
-   // current user has grading rights and include[]=needs_grading_count
-   "needs_grading_count": 17,
-   // optional: the enrollment term object for the course returned only if
-   // include[]=term
-   "term": null,
-   // optional: information on progress through the course returned only if
-   // include[]=course_progress
-   "course_progress": null,
-   // weight final grade based on assignment group percentages
-   "apply_assignment_group_weights": true,
-   // optional: the permissions the user has for the course. returned only for a
-   // single course and include[]=permissions
-   "permissions": {"create_discussion_topic":true,"create_announcement":true},
-   "is_public": true,
-   "is_public_to_auth_users": true,
-   "public_syllabus": true,
-   "public_syllabus_to_auth": true,
-   // optional: the public description of the course
-   "public_description": "Come one, come all to InstructureCon 2012!",
-   "storage_quota_mb": 5,
-   "storage_quota_used_mb": 5,
-   "hide_final_grades": false,
-   "license": "Creative Commons",
-   "allow_student_assignment_edits": false,
-   "allow_wiki_comments": false,
-   "allow_student_forum_attachments": false,
-   "open_enrollment": true,
-   "self_enrollment": false,
-   "restrict_enrollments_to_course_dates": false,
-   "course_format": "online",
-   // optional: this will be true if this user is currently prevented from viewing
-   // the course because of date restriction settings
-   "access_restricted_by_date": false,
-   // The course's IANA time zone name.
-   "time_zone": "America/Denver",
-   // optional: whether the course is set as a Blueprint Course (blueprint fields
-   // require the Blueprint Courses feature)
-   "blueprint": true,
-   // optional: Set of restrictions applied to all locked course objects
-   "blueprint_restrictions": {"content":true,"points":true,"due_dates":false,"availability_dates":false},
-   // optional: Sets of restrictions differentiated by object type applied to
-   // locked course objects
-   "blueprint_restrictions_by_object_type": {"assignment":{"content":true,"points":true},"wiki_page":{"content":true}},
-   // optional: whether the course is set as a template (requires the Course
-   // Templates feature)
-   "template": true
+ // the unique identifier for the course
+ "id": 370663,
+ // the SIS identifier for the course, if defined. This field is only included if
+ // the user has permission to view SIS information.
+ "sis_course_id": null,
+ // the UUID of the course
+ "uuid": "WvAHhY5FINzq5IyRIJybGeiXyFkG3SqHUPb7jZY5",
+ // the integration identifier for the course, if defined. This field is only
+ // included if the user has permission to view SIS information.
+ "integration_id": null,
+ // the unique identifier for the SIS import. This field is only included if the
+ // user has permission to manage SIS information.
+ "sis_import_id": 34,
+ // the full name of the course. If the requesting user has set a nickname for
+ // the course, the nickname will be shown here.
+ "name": "InstructureCon 2012",
+ // the course code
+ "course_code": "INSTCON12",
+ // the actual course name. This field is returned only if the requesting user
+ // has set a nickname for the course.
+ "original_name": "InstructureCon-2012-01",
+ // the current state of the course, also known as ‘status’.  The value will be
+ // one of the following values: 'unpublished', 'available', 'completed', or
+ // 'deleted'.  NOTE: When fetching a singular course that has a 'deleted'
+ // workflow state value, an error will be returned with a message of 'The
+ // specified resource does not exist.'
+ "workflow_state": "available",
+ // the account associated with the course
+ "account_id": 81259,
+ // the root account associated with the course
+ "root_account_id": 81259,
+ // the enrollment term associated with the course
+ "enrollment_term_id": 34,
+ // A list of grading periods associated with the course
+ "grading_periods": null,
+ // the grading standard associated with the course
+ "grading_standard_id": 25,
+ // the grade_passback_setting set on the course
+ "grade_passback_setting": "nightly_sync",
+ // the date the course was created.
+ "created_at": "2012-05-01T00:00:00-06:00",
+ // the start date for the course, if applicable
+ "start_at": "2012-06-01T00:00:00-06:00",
+ // the end date for the course, if applicable
+ "end_at": "2012-09-01T00:00:00-06:00",
+ // the course-set locale, if applicable
+ "locale": "en",
+ // A list of enrollments linking the current user to the course. for student
+ // enrollments, grading information may be included if include[]=total_scores
+ "enrollments": null,
+ // optional: the total number of active and invited students in the course
+ "total_students": 32,
+ // course calendar
+ "calendar": null,
+ // the type of page that users will see when they first visit the course -
+ // 'feed': Recent Activity Dashboard - 'wiki': Wiki Front Page - 'modules':
+ // Course Modules/Sections Page - 'assignments': Course Assignments List -
+ // 'syllabus': Course Syllabus Page other types may be added in the future
+ "default_view": "feed",
+ // optional: user-generated HTML for the course syllabus
+ "syllabus_body": "<p>syllabus html goes here</p>",
+ // optional: the number of submissions needing grading returned only if the
+ // current user has grading rights and include[]=needs_grading_count
+ "needs_grading_count": 17,
+ // optional: the enrollment term object for the course returned only if
+ // include[]=term
+ "term": null,
+ // optional: information on progress through the course returned only if
+ // include[]=course_progress
+ "course_progress": null,
+ // weight final grade based on assignment group percentages
+ "apply_assignment_group_weights": true,
+ // optional: the permissions the user has for the course. returned only for a
+ // single course and include[]=permissions
+ "permissions": {"create_discussion_topic":true,"create_announcement":true},
+ "is_public": true,
+ "is_public_to_auth_users": true,
+ "public_syllabus": true,
+ "public_syllabus_to_auth": true,
+ // optional: the public description of the course
+ "public_description": "Come one, come all to InstructureCon 2012!",
+ "storage_quota_mb": 5,
+ "storage_quota_used_mb": 5,
+ "hide_final_grades": false,
+ "license": "Creative Commons",
+ "allow_student_assignment_edits": false,
+ "allow_wiki_comments": false,
+ "allow_student_forum_attachments": false,
+ "open_enrollment": true,
+ "self_enrollment": false,
+ "restrict_enrollments_to_course_dates": false,
+ "course_format": "online",
+ // optional: this will be true if this user is currently prevented from viewing
+ // the course because of date restriction settings
+ "access_restricted_by_date": false,
+ // The course's IANA time zone name.
+ "time_zone": "America/Denver",
+ // optional: whether the course is set as a Blueprint Course (blueprint fields
+ // require the Blueprint Courses feature)
+ "blueprint": true,
+ // optional: Set of restrictions applied to all locked course objects
+ "blueprint_restrictions": {"content":true,"points":true,"due_dates":false,"availability_dates":false},
+ // optional: Sets of restrictions differentiated by object type applied to
+ // locked course objects
+ "blueprint_restrictions_by_object_type": {"assignment":{"content":true,"points":true},"wiki_page":{"content":true}},
+ // optional: whether the course is set as a template (requires the Course
+ // Templates feature)
+ "template": true
  }
  */
 import Foundation
+
 
 struct Course: Codable, Equatable, Hashable {
     let id: Int?
@@ -173,7 +174,7 @@ struct Course: Codable, Equatable, Hashable {
     let blueprintRestrictions: [String: Bool]?
     let blueprintRestrictionsByObjectType: [String: [String: Bool]]?
     let template: Bool?
-
+    
     enum CodingKeys: String, CodingKey {
         case id
         case sisCourseID = "sis_course_id"
@@ -232,7 +233,7 @@ struct Course: Codable, Equatable, Hashable {
 struct Permissions: Codable, Equatable, Hashable {
     let createDiscussionTopic: Bool
     let createAnnouncement: Bool
-
+    
     enum CodingKeys: String, CodingKey {
         case createDiscussionTopic = "create_discussion_topic"
         case createAnnouncement = "create_announcement"
@@ -241,4 +242,12 @@ struct Permissions: Codable, Equatable, Hashable {
 
 struct CalendarLink: Codable, Equatable, Hashable {
     let ics: String
+}
+
+extension Course {
+    init?(from data:Data) {
+        self = try! JSONDecoder().decode(Course.self, from: data)
+    }
+    
+    
 }
