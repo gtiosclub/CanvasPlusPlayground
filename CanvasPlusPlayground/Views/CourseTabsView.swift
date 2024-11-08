fc20b7a247f7225b399c6f896d69cd64a274b1de
Max <maxwangko1@gmail.com>
2024-11-08 17:09:10 -0500

    render tabs with sheet

--- CanvasPlusPlayground/Views/CourseTabsView.swift
+++ CanvasPlusPlayground/Views/CourseTabsView.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        var request = URLRequest(url: url)
+        
+        // TODO: pass auth token, not accessToken
+        request.setValue("Bearer \(StorageKeys.accessTokenValue)", forHTTPHeaderField: "Authorization")
+        webView.load(request)
+    }
+}
 
 struct CourseTabsView: View {
     let course: Course
     let base_url: String
     @State var tabsManager: CourseTabsManager
     
+    @State private var selectedURL: URL?
+    @State private var showWebView = false
+
     init(course: Course) {
         self.course = course
         self.tabsManager = CourseTabsManager(course: course)
@@ -20,16 +40,37 @@ struct CourseTabsView: View {
     
     var body: some View {
         List(tabsManager.tabLabels, id: \.self) { label in
-            let lower_case_label = label.lowercased()
-            let urlString = label != "home" ? base_url + "/\(lower_case_label)" : base_url
-            
-            if let url = URL(string: urlString) {
-                Link(label, destination:url)
+            Button(label) {
+                let lower_case_label = label.lowercased()
+                let urlString = (lower_case_label == "home") ? base_url : "\(base_url)/\(lower_case_label)"
+                
+                if let url = URL(string: urlString) {
+                    selectedURL = url
+                    showWebView = true
+                }
             }
         }
         .navigationTitle("Tabs")
         .task {
             await tabsManager.fetchTabs()
+        }
+        .sheet(isPresented: $showWebView) {
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Button("Close") {
+                        showWebView = false
+                    }
+                    .padding(.top, 15)
+                    .padding(.trailing, 20)
+                }
+                
+                if let url = selectedURL {
+                    WebView(url: url)
+                }
+                Spacer()
+            }
         }
     }
 }
