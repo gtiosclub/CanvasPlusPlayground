8259ac3e50d73548ae9a66b537b96d0d11ebf863
Max <maxwangko1@gmail.com>
2024-11-08 16:37:08 -0500

    tabs as external link

--- CanvasPlusPlayground/Views/CourseTabsView.swift
+++ CanvasPlusPlayground/Views/CourseTabsView.swift
@@ -9,16 +9,23 @@ import SwiftUI
 
 struct CourseTabsView: View {
     let course: Course
+    let base_url: String
     @State var tabsManager: CourseTabsManager
     
     init(course: Course) {
         self.course = course
         self.tabsManager = CourseTabsManager(course: course)
+        self.base_url = "https://gatech.instructure.com/courses/\(String(course.id!))"
     }
     
     var body: some View {
         List(tabsManager.tabLabels, id: \.self) { label in
-            Text(label)
+            let lower_case_label = label.lowercased()
+            let urlString = label != "home" ? base_url + "/\(lower_case_label)" : base_url
+            
+            if let url = URL(string: urlString) {
+                Link(label, destination:url)
+            }
         }
         .navigationTitle("Tabs")
         .task {
