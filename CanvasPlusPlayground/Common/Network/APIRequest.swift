1e92ef6575ac29ef684e738e302a0850dd18f713
Abdulaziz Albahar <abdulaziz@albahar.net>
2024-12-18 12:29:02 -0500

    protocol made

---
+++ CanvasPlusPlayground/Common/Network/APIRequest.swift
@@ -0,0 +1,78 @@
+//
+//  APIRequest.swift
+//  CanvasPlusPlayground
+//
+//  Created by Abdulaziz Albahar on 12/17/24.
+//
+
+import Foundation
+
+protocol APIRequest {
+    associatedtype Subject: Codable
+    associatedtype QueryResult = Subject
+    
+    typealias QueryParameter = (name: String, value: String?)
+    
+    var path: String { get }
+    var queryParameters: [QueryParameter] { get }
+    
+    var requestId: String? { get }
+    var requestIdKey: KeyPath<Subject, String>? { get }
+}
+
+extension APIRequest {
+    var baseURL: URL {
+        URL(string: "https://gatech.instructure.com/api/v1")!
+    }
+    
+    var url: URL {
+        baseURL
+            .appendingPathComponent(path)
+            .appending(queryItems: ([(name: "access_token", value: StorageKeys.accessTokenValue)] + queryParameters).map { name, val in
+                URLQueryItem(name: name, value: val)
+            })
+    }
+}
+
+extension APIRequest {
+    var yieldsCollection: Bool {
+        QueryResult.self is any Collection.Type
+    }
+}
+
+protocol ArrayAPIRequest: APIRequest {
+    associatedtype QueryResult = [Subject]
+}
+
+struct GetCoursesRequest: ArrayAPIRequest {
+    typealias Subject = Course
+    
+    var path: String { "courses" }
+    var queryParameters: [QueryParameter] {
+        [
+            ("enrollment_state", enrollmentState),
+            ("per_page", perPage)
+        ]
+    }
+    
+    let enrollmentState: String = "active"
+    let perPage: String = "50"
+    
+    var requestId: String? { "courses_\(StorageKeys.accessTokenValue)" }
+    var requestIdKey: KeyPath<Course, String>? { \.parentId }
+}
+
+
+struct GetCourseRequest: APIRequest {
+    typealias Subject = Course
+    
+    let courseId: String
+    
+    var path: String { "courses/\(courseId)" }
+    var queryParameters: [QueryParameter] {
+        []
+    }
+    
+    var requestId: String? { courseId }
+    var requestIdKey: KeyPath<Course, String>? { \.id }
+}
