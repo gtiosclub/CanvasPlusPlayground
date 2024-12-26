1e4c5dae23f8e15b81314b080e863d7dbc4ed44e
Rahul Narayanan <rahulon@icloud.com>
2024-12-26 11:55:34 +0530

    Revert CourseManager

--- CanvasPlusPlayground/Features/Courses/CourseManager.swift
+++ CanvasPlusPlayground/Features/Courses/CourseManager.swift
@@ -22,7 +22,7 @@ class CourseManager {
     func getCourses() async {
         do {
             let courses: [Course] = try await CanvasService.shared.loadAndSync(
-                CanvasRequest.getCourses(enrollmentState: "completed"),
+                CanvasRequest.getCourses(enrollmentState: "active"),
                 onCacheReceive: { cachedCourses in
                     guard let cachedCourses else { return }
                     setCourses(cachedCourses)
