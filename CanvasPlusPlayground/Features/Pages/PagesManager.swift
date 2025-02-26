2cd51b609f83bed3c87bdc9e40f963a0641b250d
Max <maxwangko1@gmail.com>
2025-02-25 23:49:16 -0500

    Pages Tab to list all pages and render HTML of each page

---
+++ CanvasPlusPlayground/Features/Pages/PagesManager.swift
@@ -0,0 +1,28 @@
+//
+//  PagesManager.swift
+//  CanvasPlusPlayground
+//
+//  Created by Max Ko on 2/25/25.
+//
+
+import Foundation
+
+@Observable
+class PagesManager {
+    private let courseID: String
+    var pages = [Page]()
+
+    init(courseID: String) {
+        self.courseID = courseID
+    }
+
+    func fetchPages() async {
+        let request = CanvasRequest.getPages(courseId: "268514")
+        do {
+            let fetchedPages = try await CanvasService.shared.loadAndSync(request)
+            self.pages = fetchedPages
+        } catch {
+            LoggerService.main.error("Failed to fetch pages: \(error)")
+        }
+    }
+}
