0d054ef3339a34fddb34057f1804b1ef01d9d232
ethan fox <ethanfox646@icloud.com>
2025-03-20 18:06:11 -0400

    forgot a file for assignment submissions

--- CanvasPlusPlayground/Common/Network/API Requests/SubmitAssignmentRequest.swift
+++ CanvasPlusPlayground/Common/Network/API Requests/SubmitAssignmentRequest.swift
@@ -10,54 +10,34 @@ import Foundation
 struct SubmitAssignmentRequest: NoReturnAPIRequest {
     let courseID: String
     let assignmentID: String
-    let submissionType: Assignment.SubmissionType
-    
-    let textComment: String?
-    let groupComment: String?
-    let body: String?
-    let url: String?
-    let fileIds: [Int]?
-    let mediaCommentId: String?
-    let mediaCommentType: String?
-    let userId: Int?
-    let annotatableAttachmentIds: Int?
-    let submittedAt: Date?
-    
-    
-    init(courseID: String,
-         assignmentID: String,
-         submissionType: Assignment.SubmissionType,
-         textComment: String? = nil,
-         groupComment: String? = nil,
-         body: String? = nil,
-         url: String? = nil,
-         fileIds: [Int]? = nil,
-         mediaCommentId: String? = nil,
-         mediaCommentType: String? = nil,
-         userId: Int? = nil,
-         annotatableAttachmentIds: Int? = nil,
-         submittedAt: Date? = nil,
-         queryParameters: [QueryParameter] = []
-    ) {
-        self.courseID = courseID
-        self.assignmentID = assignmentID
-        self.submissionType = submissionType
-        self.textComment = textComment
-        self.groupComment = groupComment
-        self.body = body
-        self.url = url
-        self.fileIds = fileIds
-        self.mediaCommentId = mediaCommentId
-        self.mediaCommentType = mediaCommentType
-        self.userId = userId
-        self.annotatableAttachmentIds = annotatableAttachmentIds
-        self.submittedAt = submittedAt
-        self.queryParameters = queryParameters
-    }
-    
+
     var path: String {
-        "/api/v1/courses/\(courseID)/assignments/\(assignmentID)/submissions"
+        "courses/\(courseID)/assignments/\(assignmentID)/submissions"
     }
-    
+
+    let textComment: String?
+    let submissionType: SubmissionType
+    let submission_body: String?
+    let url: String?
+    let fileIDs: [Int]?
     var queryParameters: [QueryParameter] = []
+    var method:RequestMethod { .POST }
+    
+    var body: Data? {
+        let dict: [String: Any] = [
+            "submission": [
+                "submission_type": submissionType.rawValue,
+                "text_comment": textComment,
+                "body": submission_body,
+                "file_ids": fileIDs,
+                "url": url,
+            ]
+        ]
+        if let jsonData = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted) {
+            print(jsonData)
+            return jsonData
+        }
+        
+        return nil
+    }
 }
