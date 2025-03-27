52f984f21f1f3075d648e0f3400c3371675207a4
Rahul Narayanan <rahulon@icloud.com>
2025-03-27 13:14:15 -0400

    Prepare for Testflight (#284)

--- CanvasPlusPlayground/Common/Utilities/URL+AppRootURL.swift
+++ CanvasPlusPlayground/Common/Utilities/URL+AppRootURL.swift
@@ -11,23 +11,18 @@ extension URL {
     static var appRootURL: URL? {
         let fileManager = FileManager.default
 
-        if let bundleId = Bundle.main.bundleIdentifier {
-            let root = URL.applicationSupportDirectory.appendingPathComponent(bundleId)
+        let root = URL.applicationSupportDirectory
 
-            if fileManager.fileExists(atPath: root.path) {
-                return root
-            } else {
-                do {
-                    try fileManager.createDirectory(at: root, withIntermediateDirectories: true, attributes: nil)
-                    return root
-                } catch {
-                    LoggerService.main.error("Failure creating directory to root.")
-                    return nil
-                }
-            }
+        if fileManager.fileExists(atPath: root.path) {
+            return root
         } else {
-            LoggerService.main.error("Failure getting bundle identifier")
-            return nil
+            do {
+                try fileManager.createDirectory(at: root, withIntermediateDirectories: true, attributes: nil)
+                return root
+            } catch {
+                LoggerService.main.error("Failure creating directory to root.")
+                return nil
+            }
         }
     }
 }
