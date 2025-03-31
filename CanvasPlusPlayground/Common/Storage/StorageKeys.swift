59d0b02fee365bbe5674c7e7a47276c38204dd89
Aziz Albahar <abdulaziz@albahar.net>
2025-03-31 14:27:44 -0400

    temp userdefaults to keychain switching logic

--- CanvasPlusPlayground/Common/Storage/StorageKeys.swift
+++ CanvasPlusPlayground/Common/Storage/StorageKeys.swift
@@ -8,56 +8,72 @@
 import Foundation
 
 enum StorageKeys {
-    static let accessToken = "com.canvasPlus.AccessToken"
-    static let installedModels = "com.canvasPlus.installedModels"
+    static let accessTokenKey = "com.canvasPlus.AccessToken"
+    static let installedModelsKey = "com.canvasPlus.installedModels"
 
     static let baseKeychainQuery = [
         kSecClass as String: kSecClassGenericPassword,
-        kSecAttrService as String: accessToken,
-        kSecAttrAccount as String: accessToken
+        kSecAttrService as String: accessTokenKey,
+        kSecAttrAccount as String: accessTokenKey
     ] as [String : Any]
 
     static var accessTokenValue: String {
         get {
-            let query: [String: Any] = [
-                kSecMatchLimit as String: kSecMatchLimitOne,
-                kSecReturnData as String: true
-            ].merging(baseKeychainQuery, uniquingKeysWith: { _, _ in })
-
-            var item: CFTypeRef?
-            let status = SecItemCopyMatching(query as CFDictionary, &item)
-
-            if status == errSecSuccess, let data = item as? Data, let token = String(data: data, encoding: .utf8) {
-                return token
-            } else {
-                LoggerService.main.error("[StorageKeys] Failed to retrieve access token.")
-                return ""
+            // TODO: remove this block after April 10
+            // for beta users that have tokens in UserDefaults
+            if let token = UserDefaults.standard.string(forKey: accessTokenKey) {
+                UserDefaults.standard.removeObject(forKey: accessTokenKey)
+                setNewAccessToken(token)
             }
+            // dont remove after this line
+
+            return getAccessToken()
         }
 
         set {
-            SecItemDelete(baseKeychainQuery as CFDictionary)
+            setNewAccessToken(newValue)
+        }
+    }
 
-            guard !newValue.isEmpty else {
-                return
-            }
+    static func getAccessToken() -> String {
+        let query: [String: Any] = [
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnData as String: true
+        ].merging(baseKeychainQuery, uniquingKeysWith: { _, _ in })
 
-            guard let tokenData = newValue.data(using: .utf8) else {
-                LoggerService.main.error("[StorageKeys] Failed to convert token to data")
-                return
-            }
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
 
-            let addQuery: [String: Any] = [
-                kSecValueData as String: tokenData,
-                kSecAttrAccessible as String: kSecAttrAccessibleWhenUnlockedThisDeviceOnly
-            ].merging(baseKeychainQuery, uniquingKeysWith: { _, _ in })
+        if status == errSecSuccess, let data = item as? Data, let token = String(data: data, encoding: .utf8) {
+            return token
+        } else {
+            LoggerService.main.error("[StorageKeys] Failed to retrieve access token.")
+            return ""
+        }
+    }
 
-            let status = SecItemAdd(addQuery as CFDictionary, nil)
+    static func setNewAccessToken(_ token: String) {
+        SecItemDelete(baseKeychainQuery as CFDictionary)
 
-            guard status == errSecSuccess else {
-                LoggerService.main.error("[StorageKeys] Failed to store access token.")
-                return
-            }
+        guard !token.isEmpty else {
+            return
+        }
+
+        guard let tokenData = token.data(using: .utf8) else {
+            LoggerService.main.error("[StorageKeys] Failed to convert token to data")
+            return
+        }
+
+        let addQuery: [String: Any] = [
+            kSecValueData as String: tokenData,
+            kSecAttrAccessible as String: kSecAttrAccessibleWhenUnlockedThisDeviceOnly
+        ].merging(baseKeychainQuery, uniquingKeysWith: { _, _ in })
+
+        let status = SecItemAdd(addQuery as CFDictionary, nil)
+
+        guard status == errSecSuccess else {
+            LoggerService.main.error("[StorageKeys] Failed to store access token.")
+            return
         }
     }
 
