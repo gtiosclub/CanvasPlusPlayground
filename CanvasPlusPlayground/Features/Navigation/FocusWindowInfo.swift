817996dffa8c52bd6d088cd08c7ecb8b9cc95d21
Steven Liu <lsystevee@gmail.com>
2025-09-01 17:45:39 -0400

    [save progress] allowed a subpage of a course to open a new focus window, with a critical bug to be fixed

---
+++ CanvasPlusPlayground/Features/Navigation/FocusWindowInfo.swift
@@ -0,0 +1,11 @@
+//
+//  Course+Page.swift
+//  CanvasPlusPlayground
+//
+//  Created by Steven Liu on 9/1/25.
+//
+
+struct FocusWindowInfo: Codable {
+    let courseID: Course.ID
+    let coursePage: NavigationModel.CoursePage
+}
---
+++ CanvasPlusPlayground/Features/Navigation/FocusWindowView.swift
@@ -0,0 +1,18 @@
+//
+//  FocusWindowView.swift
+//  CanvasPlusPlayground
+//
+//  Created by Steven Liu on 9/1/25.
+//
+
+import SwiftUI
+
+struct FocusWindowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusWindowView()
+}
