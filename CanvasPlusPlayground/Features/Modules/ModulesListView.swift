b394745f009159e92bb5a0e15f818643d5aa1e0c
Rahul Narayanan <rahulon@icloud.com>
2025-09-02 14:51:48 -0400

    basic module detail navigation

--- CanvasPlusPlayground/Features/Modules/ModulesListView.swift
+++ CanvasPlusPlayground/Features/Modules/ModulesListView.swift
@@ -17,7 +17,7 @@ struct ModulesListView: View {
     }
 
     var body: some View {
-        List(modulesVM.moduleBlocks) {block in
+        List(modulesVM.moduleBlocks) { block in
             ModuleSection(moduleBlock: block)
         }
         .task {
@@ -27,6 +27,7 @@ struct ModulesListView: View {
         }
         .statusToolbarItem("Modules", isVisible: isLoadingModules)
         .environment(modulesVM)
+        .navigationTitle("Modules")
     }
 }
 
@@ -37,6 +38,7 @@ private struct ModuleSection: View {
     var moduleItems: [ModuleItem]
 
     @Environment(ModulesViewModel.self) private var modulesVM
+    @State private var showPrerequisites = false
 
     init(moduleBlock: ModuleBlock) {
         self.module = moduleBlock.module
@@ -49,6 +51,7 @@ private struct ModuleSection: View {
             content: {
                 ForEach(moduleItems) { item in
                     ModuleItemCell(item: item)
+                        .tag(item)
                 }
             },
             label: {
@@ -60,28 +63,75 @@ private struct ModuleSection: View {
     var title: some View {
         HStack {
             Text(module.name)
+                .font(.title3)
+                .bold()
 
             Spacer()
 
-            Text("Prerequisites: \(prerequisites)")
+            Button("Show Prerequisites", systemImage: "info.circle") {
+                showPrerequisites.toggle()
+            }
+            .popover(isPresented: $showPrerequisites) {
+                prerequisitesView
+                    .presentationCompactAdaptation(.popover)
+            }
+            .buttonStyle(.plain)
+            .labelStyle(.iconOnly)
         }
-        .font(.title3)
-        .bold()
     }
 
-    var prerequisites: String {
-        modulesVM.prerequisites(for: module).map(\.name).joined(separator: ", ")
+    var prerequisitesView: some View {
+        VStack(alignment: .leading) {
+            if prerequisites.isEmpty {
+                Text("No Prerequisites").bold()
+            } else {
+                Text("Prerequisites").bold()
+                ForEach(prerequisites) { prereq in
+                    Text(prereq.name)
+                        .fontWeight(.light)
+                }
+            }
+        }
+        .padding()
+    }
+
+    var prerequisites: [Module] {
+        modulesVM.prerequisites(for: module)
     }
 }
 
 private struct ModuleItemCell: View {
+    @Environment(NavigationModel.self) private var navigationModel
+    @Environment(ModulesViewModel.self) private var modulesVM
+
     @Bindable var item: ModuleItem
+
     var indent: CGFloat {
         CGFloat(item.indent * 10)
     }
 
+    var urlServiceResult: CanvasURLService.URLServiceResult? {
+        .init(from: item.type)
+    }
+
     var body: some View {
-        Text(item.title)
-            .padding(.leading, indent)
+        Button(
+            item.title,
+            systemImage: urlServiceResult?.systemImageName ?? "square.dashed"
+        ) {
+            if let urlServiceResult {
+                Task {
+                    await navigationModel
+                        .handleURLSelection(
+                            result: urlServiceResult,
+                            courseID: modulesVM.courseID)
+                }
+            }
+        }
+        .buttonStyle(.plain)
+        .foregroundStyle(.primary)
+        .disabled(urlServiceResult == nil)
+        .padding(.leading, indent)
+        .padding(.vertical, 4)
     }
 }
