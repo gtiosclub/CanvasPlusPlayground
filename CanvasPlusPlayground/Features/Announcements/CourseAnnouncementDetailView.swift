1af1885bd91ef8e423b00a8ccfeae2531d57c3d2
Rahul Narayanan <rahulon@icloud.com>
2025-09-06 14:52:05 -0400

    Revised Deeplink Handling

--- CanvasPlusPlayground/Features/Announcements/CourseAnnouncementDetailView.swift
+++ CanvasPlusPlayground/Features/Announcements/CourseAnnouncementDetailView.swift
@@ -43,6 +43,7 @@ struct CourseAnnouncementDetailView: View {
             Section("Announcement Message") {
                 HTMLTextView(htmlText: announcement.message ?? "")
             }
+            .handleDeepLinks(for: announcement.courseId ?? "")
         }
         .formStyle(.grouped)
         .onAppear {
