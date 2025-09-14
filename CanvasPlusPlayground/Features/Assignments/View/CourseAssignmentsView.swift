32f59987406c0babf3fab82982b8024f5fe167d4
ivanli05 <ivan_li_hz@outlook.com>
2025-09-13 22:57:43 -0400

    Update CourseAssignmentsView.swift

--- CanvasPlusPlayground/Features/Assignments/View/CourseAssignmentsView.swift
+++ CanvasPlusPlayground/Features/Assignments/View/CourseAssignmentsView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct CourseAssignmentsView: View {
+    typealias GroupMode = CourseAssignmentManager.GroupMode
     let course: Course
     /// Display grades in each assignment row. Disables navigation to Assignment Details.
     let showGrades: Bool
@@ -18,6 +19,25 @@ struct CourseAssignmentsView: View {
     @State private var isLoadingAssignments = true
     @State private var showingGradeCalculator = false
     @State private var selectedAssignment: Assignment?
+
+    @SceneStorage("CourseAssignmentsView.currentGroupMode")
+    var currentGroupMode: GroupMode = .type
+
+    var displayedAssignmentGroups: [any AssignmentGroupCategory] {
+        switch currentGroupMode {
+        case .type:
+            assignmentManager.assignmentGroups
+        case .dueDate:
+            [
+                UpcomingAssignmentsCategory(
+                    allAssignments: assignmentManager.allAssignments
+                ),
+                PastAssignmentsCategory(
+                    allAssignments: assignmentManager.allAssignments
+                )
+            ]
+        }
+    }
 
     init(course: Course, showGrades: Bool = false) {
         self.course = course
@@ -44,7 +64,8 @@ struct CourseAssignmentsView: View {
 
     var mainbody: some View {
         List(
-            assignmentManager.assignmentGroups,
+            displayedAssignmentGroups,
+            id: \.id,
             selection: $selectedAssignment
         ) { assignmentGroup in
             Section {
@@ -67,6 +88,14 @@ struct CourseAssignmentsView: View {
             }
         }
         .toolbar {
+            ToolbarItem {
+                Menu {
+                    groupByPicker
+                } label: {
+                    Label("Group By...", systemImage: "arrow.up.arrow.down")
+                }
+            }
+
             if showGrades {
                 ToolbarItem(placement: .automatic) {
                     Button("Calculate Grades", image: .customFunctionCapsule) {
@@ -100,6 +129,17 @@ struct CourseAssignmentsView: View {
         .environment(gradeCalculator)
     }
 
+    private var groupByPicker: some View {
+        Picker("Group Assignments",
+            selection: $currentGroupMode) {
+                ForEach(GroupMode.allCases,id: \.self) { mode in
+                    Text(mode.rawValue).tag(mode)
+                }
+            }
+            .fixedSize()
+            .pickerStyle(.inline)
+    }
+
     private func loadAssignments() async {
         isLoadingAssignments = true
         await assignmentManager.fetchAssignmentGroups()
@@ -107,7 +147,6 @@ struct CourseAssignmentsView: View {
         isLoadingAssignments = false
     }
 }
-
 
 private struct AssignmentRow: View {
     @Environment(GradeCalculator.self) private var calculator
@@ -124,7 +163,9 @@ private struct AssignmentRow: View {
 
     var body: some View {
         if !showGrades {
-            NavigationLink( value: NavigationModel.Destination.assignment(assignment)) {
+            NavigationLink(
+                value: NavigationModel.Destination.assignment(assignment)
+            ) {
                 bodyContents
             }
             .contextMenu {
@@ -133,7 +174,6 @@ private struct AssignmentRow: View {
                     courseID: assignment.courseId?.asString,
                     type: .assignment
                 )
-
                 NewWindowButton(destination: .assignment(assignment))
                 OpenInCanvasButton(path: .assignment(assignment.courseId?.asString ?? "MISSING_COURSE_ID", assignment.id))
             }
@@ -195,7 +235,7 @@ private struct AssignmentRow: View {
 }
 
 private struct GroupHeader: View {
-    let assignmentGroup: AssignmentGroup
+    let assignmentGroup: any AssignmentGroupCategory
     let showGrades: Bool
 
     @State private var showingInfo = false
@@ -208,7 +248,7 @@ private struct GroupHeader: View {
 
     var body: some View {
         HStack {
-            Text(assignmentGroup.name)
+            Text(assignmentGroup.title)
 
             Spacer()
 
