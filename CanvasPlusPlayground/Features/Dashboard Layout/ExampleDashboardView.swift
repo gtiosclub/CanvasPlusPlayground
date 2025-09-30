986d05317abce4a990066264f8e0f614ececdd55
Steven Liu <lsystevee@gmail.com>
2025-09-30 17:24:11 -0400

    implemented ios dashboard layout

--- CanvasPlusPlayground/Features/Dashboard Layout/ExampleDashboardView.swift
+++ CanvasPlusPlayground/Features/Dashboard Layout/ExampleDashboardView.swift
@@ -7,9 +7,55 @@
 
 import SwiftUI
 
-struct ExampleDashboardView: View {
+// MARK: DO NOT specify the width of any widget -- the layout can figure out itself
+// MARK: DO specify the widget size of each widget using .widgetSize(:) modifier
+// MARK: Specifying the height is allowed -- for the best results, use the same height for the same type of widgets
+// MARK: Large widget will take up the entire line
+// MARK: Two medium widgets evenly take up the entire line
+// MARK: Three small widgets evenly take up the entire line
+fileprivate struct ExampleDashboardView: View {
     var body: some View {
-        Text("Hello, World!")
+        Dashboard(spacing: 10) {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.green)
+                .frame(height: 200)
+                .widgetSize(.large)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.red)
+                .frame(height: 150)
+                .widgetSize(.medium)
+
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.red)
+                .frame(height: 150)
+                .widgetSize(.medium)
+
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.red)
+                .frame(height: 150)
+                .widgetSize(.medium)
+
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.red)
+                .frame(height: 150)
+                .widgetSize(.medium)
+
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.yellow)
+                .frame(height: 200)
+                .widgetSize(.small)
+
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.yellow)
+                .frame(height: 200)
+                .widgetSize(.small)
+            RoundedRectangle(cornerRadius: 10)
+                .fill(.yellow)
+                .frame(height: 200)
+                .widgetSize(.small)
+        }
+        .padding()
     }
 }
 
