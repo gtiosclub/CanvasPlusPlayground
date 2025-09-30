75adfa61dcdb9f9d57f93f8c3853831001c61961
Steven Liu <lsystevee@gmail.com>
2025-09-30 17:23:47 -0400

    implemented ios version of dashboard

---
+++ CanvasPlusPlayground/Features/Dashboard Layout/ExampleDashboardView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  CanvasPlusPlayground
+//
+//  Created by Steven Liu on 9/30/25.
+//
+
+import SwiftUI
+
+struct ExampleDashboardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ExampleDashboardView()
+}
