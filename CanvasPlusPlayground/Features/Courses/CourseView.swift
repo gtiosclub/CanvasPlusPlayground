5c93e06503cec35d712e0768789793c6f40d9d96
Hanzhi Li <91039984+ivanli05@users.noreply.github.com>
2025-11-05 18:05:55 -0500

    Implemented pinned items for quizzes and grades (#483)
    Fixes #250 
    
    ## Changes Made
    
    - Added support for quizzes and grades
    - Cleaned up pinneditemcard for code reusability
    
    ## Screenshots (if applicable)
    <img width="1604" height="970" alt="Screenshot 2025-10-27 at 9 17 40 PM"
    src="https://github.com/user-attachments/assets/6aedfb79-790f-488a-9123-47c5bdece253"
    />
    
    
    
    ## Checklist
    - [x] I have implemented all requirements for this PR and its
    accompanying issue.
    - [x] I have verified my implementation across all edge cases.
    - [x] I have ensured my changes compile on macOS & iOS.
    - [ ] I have resolved all SwiftLint warnings.

--- CanvasPlusPlayground/Features/Courses/CourseView.swift
+++ CanvasPlusPlayground/Features/Courses/CourseView.swift
@@ -61,7 +61,23 @@ struct CourseView: View {
                         Label(page.title, systemImage: page.systemImageIcon)
                     }
                     .contextMenu {
+                        if let pinnedItemType = PinnedItem.PinnedItemType(coursePage: page) {
+                            PinButton(
+                                itemID: course.id,
+                                courseID: course.id,
+                                type: pinnedItemType
+                            )
+                        }
                         NewWindowButton(destination: .coursePage(page, course))
+                    }
+                    .swipeActions(edge: .leading) {
+                        if let pinnedItemType = PinnedItem.PinnedItemType(coursePage: page) {
+                            PinButton(
+                                itemID: course.id,
+                                courseID: course.id,
+                                type: pinnedItemType
+                            )
+                        }
                     }
                     .tag(page)
                 }
